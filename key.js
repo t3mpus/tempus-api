5e5758b049ae8942c32f6f0cdc9ebf49134742ba
wlaurance <w.laurance@gmail.com>
2013-03-11 13:27:09 -0400

    Apply tokenish
    Write more comments

--- key.js
+++ key.js
@@ -1,10 +1,23 @@
 var db = require(__dirname + '/lib/db'),
   bucket = 'keys',
-  middleware = require(__dirname + '/lib/middleware');
+  middleware = require(__dirname + '/lib/middleware'),
+  tokenish = require('tokenish'),
+  tokenishRiak = require('tokenish-riak'),
+  tgen = tokenish(tokenishRiak({bucket:'tokens', host: process.env.RIAK_HOST || '127.0.0.1'
+    , port: process.env.RIAK_PORT || 8098}));
+
+/*
+ * Module that exposes key functionality. Manage keys based on user account.
+ * These operations require password verification.
+ */
 module.exports = function(app){
   app.post('/key', middleware.verifyPassword, function(req, res){
-    res.send({
-      token:'blah'
+    tgen.createToken(req.id, function(err, token){
+      if (err) {
+        res.send(400, {error:err});
+      } else {
+        res.send({token:token});
+      }
     });
   });
 };
