8bd9e76db44bb30dff0121383e6f6691a53856a8
wlaurance <w.laurance@gmail.com>
2013-11-21 21:31:36 -0500

    Add db-migrate to node modules

---
+++ node_modules/db-migrate/lib/data_type.js
@@ -0,0 +1,14 @@
+module.exports = {
+  STRING: 'string',
+  TEXT: 'text',
+  INTEGER: 'int',
+  BIG_INTEGER: 'bigint',
+  REAL: 'real',
+  DATE_TIME: 'datetime',
+  TIME: 'time',
+  BLOB: 'blob',
+  TIMESTAMP: 'timestamp',
+  BINARY: 'binary',
+  BOOLEAN: 'boolean',
+  DECIMAL: 'decimal'
+};
