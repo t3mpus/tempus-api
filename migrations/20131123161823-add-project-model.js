bca68a56c8ad2424892e08b17992cc272d6fc200
wlaurance <w.laurance@gmail.com>
2013-11-23 11:39:26 -0500

    Add project model migration

---
+++ migrations/20131123161823-add-project-model.js
@@ -0,0 +1,19 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.createTable('projects', {
+    columns: {
+      id:           { type: 'int', primaryKey: true, autoIncrement: true },
+      name:         { type: 'string', notNull: true },
+      createdDate:  { type: 'datetime', notNull: true },
+      postHooks:    { type: 'string' }
+    },
+    ifNotExists: true
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  console.log('not dropping table projects');
+  callback();
+};
