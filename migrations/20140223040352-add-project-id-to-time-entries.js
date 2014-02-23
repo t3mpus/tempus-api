6d119b04dd90c2ad8cfe8e35d9de133c5b7d44e9
wlaurance <w.laurance@gmail.com>
2014-02-22 23:26:43 -0500

    Add fkey relation
    time_entries.projectId references project(id)

---
+++ migrations/20140223040352-add-project-id-to-time-entries.js
@@ -0,0 +1,18 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+var dbname = "time_entries"
+
+exports.up = function(db, callback) {
+  db.runSql(
+    'alter table ' + dbname + ' add column "projectId" integer references projects(id)',
+    callback
+  )
+};
+
+exports.down = function(db, callback) {
+  db.runSql(
+    'alter table ' + dbname + ' drop column "projectId"',
+    callback
+  )
+};
